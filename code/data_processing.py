e3b6a8a41403ae17a695fca7bf37eb7169a51809
Ccchuan123 <24301170022@m.fudan.edu.cn>
2026-04-09 22:03:07 +0800

    Update data_processing.py
    处理缺失值以及设计抽象数据类别编码

--- code/data_processing.py
+++ code/data_processing.py
@@ -13,9 +13,15 @@ def data_clean_1(IN_PATH,OUT_PATH):
 
 data_clean_1(config.RAW_TRAIN_DATA,config.CLEAN_TRAIN_DATA)#第一轮数据处理
 
-'''
-你写data_clean_2前删掉这个注释，只是一个提示
-后面测试集也用这两个函数处理，所以建议和我一样把读取路径和写入路径当作函数参数
-路径直接用config代码里定义的路径，参考我的，用绝对路径在别的电脑上会报错
-写完提交代码后改一下github自述文件，在里面写一下用的什么方式编码的
-'''
+def data_clean_2(IN_PATH, OUT_PATH):
+
+    data = pd.read_csv(IN_PATH)#读取第一轮处理后的数据集
+    
+    data["Sex"] = data["Sex"].map({"female": 0, "male": 1})#对性别进行编码，female-0，male-1
+    embarked_dummies = pd.get_dummies(data["Embarked"], prefix="Embarked")#生成Embarked的独热编码列，前缀为Embarked（列名变成Embarked_S/Embarked_C/Embarked_Q）
+    data = pd.concat([data, embarked_dummies], axis=1)#将独热编码横向拼接到原数据集
+    data = data.drop(columns=["Embarked"])#删除原始的Embarked列
+    
+    data.to_csv(OUT_PATH, index=False)#写入编码后数据集
+
+data_clean_2(config.CLEAN_TRAIN_DATA, config.ENCODED_TRAIN_DATA)#第二轮数据处理
